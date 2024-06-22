e583bd66ab807b377a69f791286519d0ed036ea3
agusala <agustinsala0@gmail.com>
2024-06-22 16:53:38 -0300

    work in progres

---
+++ post-generator/src/app/scripts/facebook.js
@@ -0,0 +1,14 @@
+// import fb from 'facebook-js-sdk';
+
+// fb.initialize({
+//   appId: '862976008996687',
+//   cookie: true,
+//   xfbml: true,
+//   version: 'v13.0'
+// });
+
+// export const getFacebookPosts = (pageId) => {
+//   return Facebook.api(`/${pageId}/feed`, 'get', {
+//     fields: 'id,permalink_url,created_time,message'
+//   });
+// };
---
+++ post-generator/src/app/scripts/json.js
@@ -0,0 +1,36 @@
+export const posteos = [
+    {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/1180977709921839?substory_index=1180977709921839",
+        "created_time": "2024-06-19T19:09:26+0000",
+        "id": "363755996811144_1180977709921839"
+      },
+      {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/122107111730361634?substory_index=441114142046053",
+        "created_time": "2024-06-19T18:38:44+0000",
+        "id": "363755996811144_122107111730361634",
+      },
+      {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/122107109804361634",
+        "created_time": "2024-06-19T18:35:42+0000",
+        "message": "lo mejor para tu negocio en marketing digital y desarrollo web!!!",
+        "id": "363755996811144_122107109804361634",
+      },
+      {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/122107109570361634",
+        "created_time": "2024-06-19T18:35:01+0000",
+        "message": "social up!!!!",
+        "id": "363755996811144_122107109570361634",
+      },
+      {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/122107109102361634",
+        "created_time": "2024-06-19T18:34:19+0000",
+        "message": "estamos para desarrollar e inovar lo mejor e tu negocio!!",
+        "id": "363755996811144_122107109102361634",
+      },
+      {
+        "permalink_url": "https://www.facebook.com/122107130828361634/posts/122107108592361634",
+        "created_time": "2024-06-19T18:33:33+0000",
+        "message": "impulsa tu marca!!!!",
+        "id": "363755996811144_122107108592361634",
+      }
+]
